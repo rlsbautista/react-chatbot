c0a3024adc9eddcbd478827fd4d3345d7e25816d
rlsb <61515352+rlsbautista@users.noreply.github.com>
2022-02-19 14:02:18 +0800

    Updates

---
+++ precache-manifest.d14b5250cf57d2934c1b12423d6747c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8850fb08bde4f7a16de0ca9551ba1dfb",
+    "url": "/react-chatbot/index.html"
+  },
+  {
+    "revision": "39493d036c97785a1c81",
+    "url": "/react-chatbot/static/css/main.d88e87d6.chunk.css"
+  },
+  {
+    "revision": "499c7c90860ab77ea87b",
+    "url": "/react-chatbot/static/js/2.e3ced5a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chatbot/static/js/2.e3ced5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39493d036c97785a1c81",
+    "url": "/react-chatbot/static/js/main.62de387d.chunk.js"
+  },
+  {
+    "revision": "57b536daa4ae757122e4",
+    "url": "/react-chatbot/static/js/runtime-main.c4a53657.js"
+  }
+]);
